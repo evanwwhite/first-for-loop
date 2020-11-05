da37bdfbdf9d9016000da2969d8dadb6cd890ff2
Evan White <69662443+evanwwhite@users.noreply.github.com>
2020-11-05 13:37:37 -0600

    Add files via upload

---
+++ FirstForLoop.java
@@ -0,0 +1,57 @@
+// Evan White
+import java.util.Scanner;
+public class FirstForLoop {
+
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Input a Start Value: ");
+		int start = input.nextInt();
+		
+	    System.out.println("Input a End Value: ");
+	    int end = input.nextInt();
+	    
+	    System.out.println("Input a Step Value: ");
+	    int step = input.nextInt();
+	    
+		for (int i = start; i < end; i += step)
+		{
+			System.out.println(i + " ");
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
